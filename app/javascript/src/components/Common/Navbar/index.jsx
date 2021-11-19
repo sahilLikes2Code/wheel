755ac776e0da719fcbdc5a8e7673ef032589b499
Sahil Kapoor <sahil.kapoor440@gmail.com>
2021-11-19 13:14:16 +0530

    Make crude changes to navbar

--- app/javascript/src/components/Common/Navbar/index.jsx
+++ app/javascript/src/components/Common/Navbar/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-import { Toastr } from "neetoui";
-import { withRouter } from "react-router-dom";
+import { UserCircle, Text, NeetoInsights } from "@bigbinary/neeto-icons";
+import { Toastr } from "@bigbinary/neetoui/v2";
+import { Sidebar } from "@bigbinary/neetoui/v2/layouts";
+import { withRouter, useHistory } from "react-router-dom";
 
 import authenticationApi from "apis/authentication";
 import { resetAuthTokens } from "apis/axios";
 import { useAuthDispatch } from "contexts/auth";
-
-import AccountDropdown from "./AccountDropdown";
-import NavItem from "./NavItem";
 
 const NavBar = () => {
   const authDispatch = useAuthDispatch();
@@ -23,30 +22,49 @@ const NavBar = () => {
     }
   };
 
+  let history = useHistory();
+
+  const navigateToProfileSettings = () => {
+    history.push("/my/profile");
+  };
+
   return (
-    <div className="bg-gray-100 nh-sidebar" key="sidebar">
-      <div className="nh-logo">
-        <div className="flex items-center justify-center w-8 h-8 rounded-md">
-          <i className="text-purple-500 ri-flashlight-fill ri-2x" />
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-between w-full h-full">
-        <div className="flex flex-col items-center justify-start w-full pt-4">
-          <NavItem title="Notes" link="/notes" icon="ri-file-text-line" />
-          <NavItem
-            title="Settings"
-            link="/settings"
-            icon="ri-settings-2-line"
-            subLinks={[
-              { title: "Change password", link: "/my/password/edit" },
-              { title: "My Profile", link: "/my/profile" },
-            ]}
-          />
-        </div>
-        <div className="mb-4">
-          <AccountDropdown handleLogout={handleLogout} />
-        </div>
-      </div>
+    <div className="flex flex-row items-start justify-start">
+      <Sidebar
+        appName="neetoUI"
+        isCollapsed
+        navLinks={[
+          {
+            icon: Text,
+            label: "Notes",
+            to: "/notes",
+          },
+          {
+            icon: UserCircle,
+            label: "Users",
+            to: "/users",
+          },
+          {
+            icon: NeetoInsights,
+            label: "Reports",
+            to: "#",
+          },
+        ]}
+        profileInfo={{
+          dropdownProps: [
+            {
+              label: "Edit",
+              onClick: () => navigateToProfileSettings(),
+            },
+            {
+              label: "Logout",
+              onClick: () => handleLogout(),
+            },
+          ],
+          imageUrl: "https://randomuser.me/api/portraits/women/10.jpg",
+          name: "Kieran Miller",
+        }}
+      />
     </div>
   );
 };
